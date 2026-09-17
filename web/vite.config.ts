cc935cf963dc72624b83e7c6e25ce0deeae4e93a
Yashi Rastogi <155823373+yashi2606@users.noreply.github.com>
2026-09-17 18:38:26 +0530

    Update base path in Vite configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/visor-leetcode",
+  base: "/leetcode-visor/",
   plugins: [
     react({
       babel: {
